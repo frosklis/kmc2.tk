6c2f17ef39ee49647c15ef5455ebc3fa43350b44
Claudio Noguera <claudio.noguera@gmail.com>
2019-11-11 06:42:24 +0100

    renombrado de fotos

--- scripts/picturedata.sh
+++ scripts/picturedata.sh
@@ -1,6 +1,5 @@
-
-
-cd /Users/claudio/Dropbox/webs/www.kmc2.tk/
+cd ..
 cd content
+exiftool ./ '-filename<CreateDate' -d %Y%m%d-%H%M%S%%-c.%%le -r
 echo -e "directory\tfile\tdatetime\ttitle\tdescription\tlatitude\tlongitude" > ../data-src/pictures.tsv
 exiftool ./ -R -E -T -n -directory -filename -datetimeoriginal -title -description -gpslatitude -gpslongitude >> ../data-src/pictures.tsv
