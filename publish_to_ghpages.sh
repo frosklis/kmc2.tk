33276e852f664cbaddb9786dba1aaeb3ca98ca3e
Claudio Noguera <claudio.noguera@gmail.com>
2019-11-29 23:55:19 +0100

    script de publicación

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -19,7 +19,8 @@ echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
-hugo
+HUGO_ENV="production"
+hugo -e
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
