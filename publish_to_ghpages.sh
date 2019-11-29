abe4161274dfa87785448c179c1affccc4180a00
Claudio Noguera <claudio.noguera@gmail.com>
2019-11-30 00:01:08 +0100

    merge

--- publish_to_ghpages.sh
+++ publish_to_ghpages.sh
@@ -20,7 +20,7 @@ rm -rf public/*
 
 echo "Generating site"
 HUGO_ENV="production"
-hugo -e
+hugo
 
 echo "Updating gh-pages branch"
 cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
